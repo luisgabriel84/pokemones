373592cfee698678c191599f1cc7703260d14080
Luis Gabriel Rodriguez <Luis-Gabriel.Rodriguez@endava.com>
2022-05-05 18:54:42 -0500

    pokemones

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hola pokemones</h1>
+
+
+<table border="1" width="100%">
+  <thead>
+    <tr>
+      <th>Id</th>
+      <th>Name</th>
+      <th>Height</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    <tr v-for="(pokemon, index ) in pokemonList " :key=index>
+      <td>{{pokemon.id}}</td>
+      <td> {{pokemon.name}}</td>
+      <td> {{pokemon.height}}</td>
+    </tr>
+  </tbody>
+</table>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+ data: function () {
+    return {
+      pokemonList:[]
+    }
+  },
+  methods:{
+    async  fetchPokemones(){
+        try{
+            const pokemones = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=100")
+            const pokeArray = await pokemones.json()
+           pokeArray.results.map(this.fetchPokemon)
+        }catch(error){
+            console.log(error)
+        }
+      
+    },
+
+    async fetchPokemon(){
+        const {url }= arguments[0]
+        try{
+            const pokemonResponse = await fetch(`${url}`)
+            const poke = await pokemonResponse.json()
+            console.log(poke)
+            this.pokemonList.push(poke)
+
+        }catch(error){
+            console.log(error)
+        }
+
+    }
+  },
+  created(){
+      this.fetchPokemones()
   }
+    
+  
 }
 </script>
 
@@ -24,5 +76,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+table{
+  width: 100%;
+  border: 1px solid;
 }
 </style>
